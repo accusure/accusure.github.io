208cfcf8569d5ee11ec49b8fe0fcaeedbc1aaaf2
Matthew Breeden <matt@integrallis.com>
2014-04-17 16:31:28 -0700

    update 'Contact Us' link with mailto and popover content

--- js/app.js
+++ js/app.js
@@ -30,4 +30,23 @@ $(function () {
 		pauseOnHover: true
 	});
 
+  $('.contact-popover').popover({delay: { hide: 400 }});
+
 });
+
+// Taken from http://jsfiddle.net/J9fYR/ via http://stackoverflow.com/questions/16740125
+(function($) {
+  var oldHide = $.fn.popover.Constructor.prototype.hide;
+
+  $.fn.popover.Constructor.prototype.hide = function() {
+    if (this.options.trigger === "hover" && this.tip().is(":hover")) {
+      var that = this;
+      // try again after what would have been the delay
+      setTimeout(function() {
+        return that.hide.call(that, arguments);
+      }, that.options.delay.hide);
+      return;
+    }
+    oldHide.call(this, arguments);
+  };
+})(jQuery);
