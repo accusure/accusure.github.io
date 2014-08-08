fc3ceb6192d33a3915b3f8c56aa51d0555138df0
Melody Sy <annemelodysy@gmail.com>
2014-08-08 15:05:14 +0800

    Add panel snap plugin for the sections

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
-  var $window;
-  $window = $(window);
-  return $("section[data-type=\"background\"]").each(function() {
-    var $bgobj;
-    $bgobj = $(this);
-    return $(window).scroll(function() {
-      var coords, yPos;
-      yPos = -($window.scrollTop() / $bgobj.data("speed"));
-      coords = "50% " + yPos + "px";
-      return $bgobj.css({
-        backgroundPosition: coords
-      });
-    });
+  $('.panels').panelSnap({
+    $menu: $(' .nav'),
+    slideSpeed: 300
   });
 });
