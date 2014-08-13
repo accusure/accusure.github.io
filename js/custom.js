5b53cf99888ba86598481167225725caebcc3861
Melody Sy <annemelodysy@gmail.com>
2014-08-13 22:18:36 +0800

    Implemented Mth degree design

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $('.panels').panelSnap({
-    $menu: $(' .nav'),
+    $menu: $('.nav'),
     slideSpeed: 300
   });
+  
+  $(function(){
+    $('.nav li a').on('click', function(e){
+      e.preventDefault(); // prevent link click if necessary?
+      var $thisLi = $(this).parent('li');
+      var $ul = $thisLi.parent('ul');
+      if (!$thisLi.hasClass('active')) {
+        $ul.find('li.active').removeClass('active');
+        $thisLi.addClass('active');
+      }
+    })
+  })
 });
