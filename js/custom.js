88799c49cdb82b036292832725d82c7e268c13b3
Melody Sy <annemelodysy@gmail.com>
2014-08-14 15:53:40 +0800

    Fix scrolling problem
    When you reach the #why section, disable the plugin.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
   $('.panels').panelSnap({
     $menu: $('.nav'),
-    slideSpeed: 300
+    slideSpeed: 300,
+    directionThreshold: 150
+  });
+  
+  // store the position of the element in position variable
+  var position = $('#why').offset();
+  console.log(position);
+  
+  $('.panels').scroll(function () {
+    //the current height
+    var y = $(this).scrollTop();
+  
+    if(y >= position.top){
+      $.event.special.scrollstart.enabled = false;
+    }else{
+      $.event.special.scrollstart.enabled = true;
+    }
   });
   
   //Highlight nav link on click
