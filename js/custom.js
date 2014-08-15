c181e8a455d5e43c50900412f5fffbd5d4a680e3
Melody Sy <annemelodysy@gmail.com>
2014-08-15 15:26:44 +0800

    Scrollspy & Scroll to section

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,23 @@
 $(document).ready(function() {
-  $('.panels').panelSnap({
-    $menu: $('.nav'),
-    slideSpeed: 300,
-    directionThreshold: 150
-  });
-  
-  // store the position of the element in position variable
-  var position = $('#why').offset();
-  
-  //Enable or disable snap
-  $('.panels').scroll(function () {
-    //the current height
-    var y = $(this).scrollTop();
-  
-    if(y >= position.top){
-      $.event.special.scrollstart.enabled = false;
-      $('.nav li').removeClass('active');
-      $('.nav a[href*="#why"]').parent().addClass('active');
-    }else{
-      $.event.special.scrollstart.enabled = true;
-      $('.nav li').removeClass('active');
-    }
-  });
-  
   //Highlight nav link on click
   $('ul.nav > li').click(function (e) {
     $('ul.nav > li').removeClass('active');
-    $(this).addClass('active');                
-  }); 
+    $(this).addClass('active');      
+  });  
+  
+  $('.banner').click(function (){
+    $('html').animate({
+      scrollTop: $('#banner').offset().top
+    }, 1000);
+  });
+  $('.video').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#video').offset().top
+    }, 1000);
+  });
+  $('.why').click(function (){
+    $('html, body').animate({
+      scrollTop: $('#why').offset().top
+    }, 1000);
+  });
 });
