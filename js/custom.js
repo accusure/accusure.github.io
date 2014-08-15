ec1a188fc386f10734df145736ce720a3ce24c18
Melody Sy <annemelodysy@gmail.com>
2014-08-15 17:05:00 +0800

    Returned false to remove flickering when sliding

--- js/custom.js
+++ js/custom.js
@@ -3,15 +3,18 @@ $(document).ready(function() {
     $('html').animate({
       scrollTop: $('#banner').offset().top - 80
     }, 500);
+    return false;
   });
   $('.video').click(function (){
     $('html, body').animate({
       scrollTop: $('#video').offset().top - 80
     }, 500);
+    return false;
   });
   $('.why').click(function (){
     $('html, body').animate({
       scrollTop: $('#why').offset().top - 80
     }, 500);
+    return false;
   });
 });
