962b1d6a12abea02f80353f1f7bbf5041c055b6a
Melody Sy <annemelodysy@gmail.com>
2014-08-15 16:51:41 +0800

    Add 80px offset on section when scrolling

--- js/custom.js
+++ js/custom.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
   
   $('.banner').click(function (){
     $('html').animate({
-      scrollTop: $('#banner').offset().top
+      scrollTop: $('#banner').offset().top - 80
     }, 1000);
   });
   $('.video').click(function (){
     $('html, body').animate({
-      scrollTop: $('#video').offset().top
+      scrollTop: $('#video').offset().top - 80
     }, 1000);
   });
   $('.why').click(function (){
     $('html, body').animate({
-      scrollTop: $('#why').offset().top
+      scrollTop: $('#why').offset().top - 80
     }, 1000);
   });
 });
