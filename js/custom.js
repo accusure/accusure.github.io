8a9a9ddb364ff8b73f9ecf46ac3fc474a97ed2af
Melody Sy <annemelodysy@gmail.com>
2014-08-15 17:02:32 +0800

    Scrollspy offset and slide speed

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
-  //Highlight nav link on click
-  $('ul.nav > li').click(function (e) {
-    $('ul.nav > li').removeClass('active');
-    $(this).addClass('active');      
-  });  
-  
   $('.banner').click(function (){
     $('html').animate({
       scrollTop: $('#banner').offset().top - 80
-    }, 1000);
+    }, 500);
   });
   $('.video').click(function (){
     $('html, body').animate({
       scrollTop: $('#video').offset().top - 80
-    }, 1000);
+    }, 500);
   });
   $('.why').click(function (){
     $('html, body').animate({
       scrollTop: $('#why').offset().top - 80
-    }, 1000);
+    }, 500);
   });
 });
