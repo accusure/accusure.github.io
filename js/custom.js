b6392e1bdc502666e10a178a724113fb06ce2fe2
Melody Sy <annemelodysy@gmail.com>
2014-08-16 11:34:31 +0800

    Desktop Changes

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
-  $('.banner').click(function (){
-    $('html').animate({
-      scrollTop: $('#banner').offset().top - 80
-    }, 500);
-    return false;
-  });
-  $('.video').click(function (){
-    $('html, body').animate({
-      scrollTop: $('#video').offset().top - 80
-    }, 500);
-    return false;
-  });
-  $('.why').click(function (){
-    $('html, body').animate({
-      scrollTop: $('#why').offset().top - 80
-    }, 500);
-    return false;
+  // Attach the click event
+  $('a[href*=#]').bind("click", function(e) {
+    e.preventDefault();
+  
+    var target = $(this).attr("href"); //Get the target
+    var scrollToPosition = $(target).offset().top - 80;
+  
+    $('html').animate({ 'scrollTop': scrollToPosition }, 1000, function(){
+      window.location.hash = "" + target;
+      // This hash change will jump the page to the top of the div with the same id
+      // so we need to force the page to back to the end of the animation
+      $('html').animate({ 'scrollTop': scrollToPosition }, 0);
+    });
   });
   
   //Close Nav on mobile click
   $('.nav a').on('click', function(){
-    $(".btn-navbar").click(); //bootstrap 2.x
-    $(".navbar-toggle").click() //bootstrap 3.x by Richard
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+  });
+  
+  //Close video when modal is closed
+  $('body').on('hidden.bs.modal', '.modal', function () {
+  $('video').trigger('pause');
   });
 });
