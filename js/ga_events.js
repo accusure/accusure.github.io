b6e7c5959c38d44ecb2d47007173cd2793109ebb
Matthew Breeden <matt@integrallis.com>
2014-08-19 03:51:13 -0700

    add event tracking for CTA buttons

---
+++ js/ga_events.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#cta1').on('click', function() {
+      ga('send', 'event', 'CTA', 'click', 'cta_1');
+  });
+  $('#cta2').on('click', function() {
+      ga('send', 'event', 'CTA', 'click', 'cta_2');
+  });
+  $('#cta3').on('click', function() {
+      ga('send', 'event', 'CTA', 'click', 'cta_3');
+  });
+  $('#cta3').on('click', function() {
+      ga('send', 'event', 'CTA', 'click', 'cta_3');
+  });
+});
